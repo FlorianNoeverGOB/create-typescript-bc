1ad62fb3ebc938e917e92941352a8b259f74b650
Florian Noever <florian.noever@gob.de>
2025-11-05 16:42:28 +0100

    updated scope

--- utils/allowedPackages.js
+++ utils/allowedPackages.js
@@ -1,11 +1,11 @@
-export const scope = 'FlorianNoeverGOB';
+export const scope = 'Florian-Noever';
 export const projectTypes = [
     'typescript-bc',
     'react-ts-bc'
 ];
 
 export const managers = {
-    npm:  { check: 'npm --version',  install: 'npm install',  lock: 'package-lock.json' },
+    npm: { check: 'npm --version', install: 'npm install', lock: 'package-lock.json' },
     yarn: { check: 'yarn --version', install: 'yarn install', lock: 'yarn.lock' },
     pnpm: { check: 'pnpm --version', install: 'pnpm install', lock: 'pnpm-lock.yaml' }
 };
